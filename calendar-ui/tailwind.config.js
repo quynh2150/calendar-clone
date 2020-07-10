59ea8ec2b251b39c3eb8cb2161961eeb35203c03
Quynh <quynhtran2150@gmail.com>
2020-07-10 18:35:57 +0700

    Init project

---
+++ calendar-ui/tailwind.config.js
@@ -0,0 +1,10 @@
+// configuring and customizing your Tailwind installation.: https://tailwindcss.com/docs/configuration/
+
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
